641654a2b669247420cd0707237fc13a851aba30
Vanshika1105 <sarjarevk@gmail.com>
2025-10-02 23:51:22 +0530

    Update count.c

--- count.c
+++ count.c
@@ -5,9 +5,6 @@ int main() {
   printf("Enter an integer: ");
   scanf("%lld", &n);
  
-  // iterate at least once, then until n becomes 0
-  // remove last digit from n in each iteration
-  // increase count by 1 in each iteration
   do {
     n /= 10;
     ++count;
@@ -15,3 +12,4 @@ int main() {
 
   printf("Number of digits: %d", count);
 }
+
