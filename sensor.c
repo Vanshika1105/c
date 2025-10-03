0e01f4c6538cee9ccaeaa61797ecb667f3adfb6f
Vanshika1105 <sarjarevk@gmail.com>
2025-10-03 22:27:01 +0530

    Add files via upload

---
+++ sensor.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+void main()
+{
+    float A,B,sum;
+       printf("Enter the temp of sensor 1\n");
+       scanf("%f",&A);
+
+        printf("Enter the temp of sensor 2\n");
+        scanf("%f",&B);
+
+        sum=A+B;
+
+        printf("total temperature of the sensor is: %2f", sum);
+
+
+    }    
