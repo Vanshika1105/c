c92a6b89cd57aada7357d56fc1c66c7aaab4d5e6
Vanshika1105 <sarjarevk@gmail.com>
2025-10-04 00:45:45 +0530

    Add files via upload

---
+++ student structure.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+struct Student {
+    char name[50];
+    int age;
+    float marks;
+};
+
+int main() {
+    struct Student s1 = {"Alice", 20, 88.5};
+
+    printf("Name: %s\n", s1.name);
+    printf("Age: %d\n", s1.age);
+    printf("Marks: %.2f\n", s1.marks);
+
+    return 0;
+}
